26a23847ed7d65b61406067676eb0b7cbe871861
JuanRestrepo <juanparpola@gmail.com>
2025-02-28 12:08:57 -0500

    prueba 2

--- PROYECTO_PROGRAMACION_DE_SOFTWARE/Program.cs
+++ PROYECTO_PROGRAMACION_DE_SOFTWARE/Program.cs
@@ -12,7 +12,7 @@ namespace PROYECTO_PROGRAMACION_DE_SOFTWARE
         {
             Console.WriteLine("Sistema de prestamos");
             Console.WriteLine("Prueba");
-            Console.WriteLine();
+            Console.WriteLine("Prueba 2");
         }
     }
 }
