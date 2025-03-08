3991986b514c259aad2c88643f65ddff553e24c1
Osdanrey <osdanrey@gmail.com>
2025-03-08 11:33:53 -0500

    Se agrego el último menú, el menú de materiales, todos los menús de administrador quedaron con relación y funcionales.

---
+++ PROYECTO_PROGRAMACION_DE_SOFTWARE/MaterialMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_PROGRAMACION_DE_SOFTWARE
+{
+    internal class MaterialMethods : IMaterialManagement
+    {
+        public void SearchMaterial()
+        {
+            Console.WriteLine("Buscando material...");
+        }
+        public void CreateMaterial()
+        {
+            Console.WriteLine("Creando material...");
+        }
+        public void UpdateMaterial()
+        {
+            Console.WriteLine("Actualizando material...");
+        }
+        public void DeleteMaterial()
+        {
+            Console.WriteLine("Eliminando material...");
+        }
+        public void ViewAvaraibleMaterials()
+        {
+            Console.WriteLine("Viendo materiales disponibles...");
+        }
+    }
+}
